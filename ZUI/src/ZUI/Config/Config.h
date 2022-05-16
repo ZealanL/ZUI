8875c3b49ae2e372a45876147ea4eb7550b9e07d
Zealan <zlaferte@gmail.com>
2022-05-16 14:52:19 -0700

    Create ZUI::Config struct

---
+++ ZUI/src/ZUI/Config/Config.h
@@ -0,0 +1,22 @@
+// Configuration state for ZUI
+
+#pragma once
+#include "../Types/Area.h"
+#include "../Types/Color.h"
+
+namespace ZUI {
+	struct Config {
+		struct {
+			Color text = Color(255, 255, 255);
+			Color outline = Color(255, 255, 255, 100);
+
+			Color windowBg = Color(30, 30, 30);
+			Color sectionBg = Color(38, 38, 38);
+		} colors;
+
+		struct {
+			float baseItemHeight = 20;
+			Vec padding = Vec(8, 8);
+		} sizes;
+	};
+}
